ebbff15a42b3e401bcb2d0e7e3f95845e39f1bda
Carmine Zaccagnino <carmine@pc-carmine.localdomain>
2017-10-01 18:55:44 +0200

    Entrambe le conversioni funzionano e l'output è più accogliente

--- conversione.py
+++ conversione.py
@@ -1,11 +1,15 @@
 #!/bin/python
 import math;
 contatore=int(1);
+i=int(0);
 resti=int(0);
+numeroout=0;
 bad=True;
 while bad==True:
-    print "Hello World!";
-    print "Hello World!!";
+    print "-------------------------------------------------------------------";
+    print "Versione Python realizzata da Carmine del";
+    print "convertitore decimale-binario binario-decimale Di Carmine e Biagio";
+    print "-------------------------------------------------------------------";
     print "";
     print "";
     print "Binario->Decimale o Decimale->Binario?";
@@ -28,11 +32,18 @@ if decbin==True:
 if bindec==True:
     print "Inserisci un numero binario da convertire in decimale";
 numeroin=input();
+numerocorrente=int(numeroin)
 if decbin==True:
-    numerocorrente=int(numeroin)
     while numerocorrente>0:
         resto=numerocorrente%2;
         resti=int(resti+resto*contatore);
         numerocorrente=int(numerocorrente/2);
         contatore=contatore*10;
     print "Il numero convertito in decimale e'",resti;
+if bindec==True:
+    while numerocorrente>0:
+        ultimacifra=numerocorrente%10;
+        numeroout=numeroout+(ultimacifra*pow(2,i));
+        numerocorrente=int(numerocorrente/10);
+        i=i+1;
+    print "Il numero convertito in binario e'",numeroout;
